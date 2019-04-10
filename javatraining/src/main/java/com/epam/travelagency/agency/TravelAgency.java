6dfcbb8596c70f6af3085a8f49736f19b7e8d252
dennistikhomirov <dennistikhomirov@gmail.com>
2019-04-10 02:43:02 -0700

    add constructor, to ]String, hashCode

--- javatraining/src/main/java/com/epam/travelagency/agency/TravelAgency.java
+++ javatraining/src/main/java/com/epam/travelagency/agency/TravelAgency.java
@@ -4,6 +4,7 @@ import com.epam.travelagency.person.Agent;
 import com.epam.travelagency.person.Customer;
 import com.epam.travelagency.tour.Tour;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Class describes travel agency: contains agents, tours, customers
@@ -14,8 +15,45 @@ import java.util.List;
  */
 public class TravelAgency {
 
-  private List<Agent> agents;
-  private List<Customer> customers;
-  private List<Tour> tours;
+  public List<Agent> agents;
+  public List<Customer> customers;
+  public List<Tour> tours;
 
+  /**
+   * Constructs {@code Person} object with {@code phoneNumber}, {@code lastName},{@code firstName}
+   *
+   * @param agents a List of Agent
+   * @param customers a List of Customers
+   * @param tours a List of Tours
+   */
+  public TravelAgency(List<Agent> agents,
+      List<Customer> customers, List<Tour> tours) {
+    this.agents = agents;
+    this.customers = customers;
+    this.tours = tours;
+  }
+
+  /**
+   * @return the hash code of a non-{@code null} argument and 0 for a {@code null} argument
+   * @see Object#hashCode
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(agents, customers, tours);
+  }
+
+  /**
+   * Returns {@code String} representation of TravelAgency object
+   *
+   * @return the result of calling {@code toString} for a non-{@code null} argument and {@code
+   * "null"} for a {@code null} argument
+   */
+  @Override
+  public String toString() {
+    return "TravelAgency{" +
+        "agents=" + agents +
+        ", customers=" + customers +
+        ", tours=" + tours +
+        '}';
+  }
 }
