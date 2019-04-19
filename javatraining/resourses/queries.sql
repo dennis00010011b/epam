6c394763a170b2f41345a33af2332daffd0093e9
dennistikhomirov <dennistikhomirov@gmail.com>
2019-04-18 23:09:27 -0700

    add databasev sql data

---
+++ javatraining/resourses/queries.sql
@@ -0,0 +1,158 @@
+// CREATING queries
+
+CREATE TABLE places (
+place VARCHAR PRIMARY KEY)
+
+create table showplaces (
+showplace VARCHAR PRIMARY KEY,
+place VARCHAR REFERENCES places
+)
+
+CREATE TABLE shops(
+shop VARCHAR PRIMARY KEY,
+place VARCHAR REFERENCES places)
+
+CREATE TABLE tours (
+Name VARCHAR PRIMARY KEY,
+price float,
+startDate date,
+endDate date,
+rating integer,
+isHot boolean)
+
+CREATE TABLE excursiontours (
+tour VARCHAR REFERENCES tours,
+showplace1 VARCHAR REFERENCES showplaces,
+showplace2 VARCHAR REFERENCES showplaces,
+showplace3 VARCHAR REFERENCES showplaces,
+showplace4 VARCHAR REFERENCES showplaces,
+showplace5 VARCHAR REFERENCES showplaces
+)
+
+CREATE TABLE shoppingtours (
+tour VARCHAR REFERENCES tours,
+shop VARCHAR REFERENCES shops
+)
+
+CREATE TABLE resorttours (
+tour VARCHAR REFERENCES tours,
+place VARCHAR REFERENCES places
+)
+
+CREATE TABLE travelagencies(
+name VARCHAR PRIMARY KEY)
+
+CREATE TABLE persons
+(   phonenumber VARCHAR(15) PRIMARY KEY,
+    firstname VARCHAR,
+    lastname VARCHAR
+)
+
+CREATE TABLE agents(
+person VARCHAR REFERENCES persons,
+bankaccount VARCHAR,
+travelagency VARCHAR REFERENCES travelagencies)
+
+CREATE TABLE customers(
+person VARCHAR REFERENCES persons,
+tour1 VARCHAR REFERENCES tours,
+tour2 VARCHAR REFERENCES tours,
+tour3 VARCHAR REFERENCES tours,
+tour4 VARCHAR REFERENCES tours,
+tour5 VARCHAR REFERENCES tours,
+isRegular boolean,
+discount float)
+
+
+// CRUD queries
+
+//CREATE queries
+
+INSERT INTO persons (phonenumber,firstname,lastname)
+VALUES ('+321', 'Griboedov', 'Ivan')
+
+INSERT INTO showplaces
+VALUES('Eifel Tower','Paris')
+
+INSERT INTO shops
+VALUES ('Fish market')
+
+INSERT INTO tours
+VALUES('Gold ring of Homel',50.4,'2019-05-03','2020-09-08',5,true)
+
+INSERT INTO excursiontours(tour,showplace1,showplace2,showplace3)
+VALUES(	'Gold ring of Homel'','Eifel Tower','Space needle','Bazar')
+
+INSERT INTO resorttours
+VALUES('Gold sand','Homel')
+
+INSERT INTO shops
+VALUES('Walmart','1st Ave S Kirkland')
+
+INSERT INTO travelagencies
+VALUES ('TravelWorld')
+
+INSERT INTO agents
+VALUES ('+321' ,'12345678987','TravelWorld')
+
+INSERT INTO customers(person,tour1,tour2,isRegular,discount)
+VALUES ('+375298887766','Elbrus','Elbrus',true,0.75)
+
+
+// READ queries
+
+SELECT * FROM persons
+WHERE phonenumber='+375298712874'
+
+SELECT firstname FROM persons
+WHERE phonenumber='+375298712874'
+
+SELECT * FROM tours,resorttours
+WHERE tour='Gold sand' AND tours.name='Gold sand'
+
+SELECT * FROM tours,excursiontours
+WHERE tours.name='Elbrus' AND excursiontours.tour='Elbrus'
+
+SELECT * FROM tours,shoppingtours
+WHERE tours.name='Elbrus' AND shoppingtours.tour='Elbrus'
+
+SELECT * FROM agents,persons
+WHERE person='+321'
+
+SELECT * FROM customers,persons
+WHERE person='+321'
+
+// UPDATE queries
+
+ALTER TABLE public.agents
+    ADD CONSTRAINT agents_person_fkey FOREIGN KEY (person)
+    REFERENCES public.persons (phonenumber) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+ALTER TABLE public.customers
+    ADD CONSTRAINT customers_person_fkey FOREIGN KEY (person)
+    REFERENCES public.persons (phonenumber) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+ALTER TABLE public.excursiontours
+    ADD CONSTRAINT excursiontours_showplace1_fkey FOREIGN KEY (showplace1)
+    REFERENCES public.showplaces (showplace) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+UPDATE persons
+SET phonenumber='+375290001122'
+WHERE firstname='Griboedov'
+
+UPDATE persons
+SET phonenumber='+375290001122'
+WHERE firstname='Griboedov'
+
+
+// DELETE  queries
+
+DELETE FROM persons
+WHERE firstname='Griboedov'
+
